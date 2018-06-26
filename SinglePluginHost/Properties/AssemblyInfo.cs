6bdb08317de318a8a174c50590944f7294af4e22
dlebansais <dlebansais@gmail.com>
2018-06-26 21:14:42 +0200

    Updated the version number

--- SinglePluginHost/Properties/AssemblyInfo.cs
+++ SinglePluginHost/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.114")]
-[assembly: AssemblyFileVersion("1.0.0.111")]
+[assembly: AssemblyVersion("1.0.0.115")]
+[assembly: AssemblyFileVersion("1.0.0.112")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
