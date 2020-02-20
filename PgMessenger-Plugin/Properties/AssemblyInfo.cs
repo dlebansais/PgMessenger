448ea9fd36ef36edb0313abd53b6a50cee1464ac
dlebansais <dlebansais@gmail.com>
2020-02-20 21:08:29 +0100

    Updated dependencies.

--- PgMessenger-Plugin/Properties/AssemblyInfo.cs
+++ PgMessenger-Plugin/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.123")]
-[assembly: AssemblyFileVersion("1.0.0.117")]
+[assembly: AssemblyVersion("1.0.0.124")]
+[assembly: AssemblyFileVersion("1.0.0.118")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
